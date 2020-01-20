c336471b9f4871d11b20f858104b84cf66616d44
mark coates <mark_coates@btinternet.com>
2020-01-20 08:50:48 +0000

    Updates

---
+++ precache-manifest.00c06cdb5ad7e9cc8807352cf2df2945.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cdf817bfd359a430ccf45a3604c769",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "e70e1858771ab4c71f91",
+    "url": "/contactmanager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ebe53ebe0334928e4215",
+    "url": "/contactmanager/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "e70e1858771ab4c71f91",
+    "url": "/contactmanager/static/js/2.16649676.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.16649676.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebe53ebe0334928e4215",
+    "url": "/contactmanager/static/js/main.0ce09294.chunk.js"
+  },
+  {
+    "revision": "6c4e0446e694a94f4cb9",
+    "url": "/contactmanager/static/js/runtime-main.cb0075d3.js"
+  }
+]);
